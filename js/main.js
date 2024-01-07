bcc54fcfca5adbdea37a3faf3048f2ccd688d329
ibn-saad <archi.ahmedsaad@gmail.com>
2024-01-07 16:05:10 +0200

    third commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,51 @@
-const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const d = new Date();
 let today = week[d.getDay()];
-let tomorrow = week[(d.getDay()+1)%7];
-let afterTomorrow = week[(d.getDay()+2)%7];
+let tomorrow = week[(d.getDay() + 1) % 7];
+let afterTomorrow = week[(d.getDay() + 2) % 7];
 
-document.querySelector('.today').innerHTML = today;
-document.querySelector('.tomorrow').innerHTML = tomorrow;
-document.querySelector('.afterTomorrow').innerHTML = afterTomorrow;
-
+document.querySelector(".today").innerHTML = today;
+document.querySelector(".tomorrow").innerHTML = tomorrow;
+document.querySelector(".afterTomorrow").innerHTML = afterTomorrow;
 
 const apikey = "7fba3e875b824190b3b113240240601";
 
 var searchBar = document.querySelector(".searchBar");
 
-const getData  = async (city) => {
-  let response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${apikey}&q=${city}&days=3`
-  );
-  let data = await response.json();
-  getToday(data);
-  getTomorrow(data);
-  getAfterTomorrow(data);
+// const getData  = async (city) => {
+//   let response = await fetch(
+//     `http://api.weatherapi.com/v1/forecast.json?key=7fba3e875b824190b3b113240240601&q=${city}&days=3`
+//   );
+//   let data = await response.json();
+//   getToday(data);
+//   getTomorrow(data);
+//   getAfterTomorrow(data);
+// };
+
+const getData = async (city) => {
+  try {
+    let response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${apikey}&q=${city}&days=3`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    let data = await response.json();
+    getToday(data);
+    getTomorrow(data);
+    getAfterTomorrow(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 function getToday(data) {
@@ -30,32 +54,37 @@ function getToday(data) {
   document.querySelector(".temp").innerHTML = data.current.temp_c + "°C";
   document.querySelector(".icon").src = data.current.condition.icon;
   document.querySelector(".condtion").innerHTML = data.current.condition.text;
-  document.querySelector(".rain").innerHTML = data.forecast.forecastday[0].day.daily_chance_of_rain + " %";
+  document.querySelector(".rain").innerHTML =
+    data.forecast.forecastday[0].day.daily_chance_of_rain + " %";
   document.querySelector(".speed").innerHTML = data.current.wind_kph + " km/h";
   document.querySelector(".dir").innerHTML = data.current.wind_dir;
-};
+}
 
 function getTomorrow(data) {
-    document.querySelector('.tIcon').src = data.forecast.forecastday[1].day.condition.icon;
-    document.querySelector('.tMaxTemp').innerHTML = data.forecast.forecastday[1].day.maxtemp_c+ "°C";
-    document.querySelector('.tMinTemp').innerHTML = data.forecast.forecastday[1].day.mintemp_c+ "°C";
-    document.querySelector('.tCond').innerHTML = data.forecast.forecastday[1].day.condition.text;
+  document.querySelector(".tIcon").src =
+    data.forecast.forecastday[1].day.condition.icon;
+  document.querySelector(".tMaxTemp").innerHTML =
+    data.forecast.forecastday[1].day.maxtemp_c + "°C";
+  document.querySelector(".tMinTemp").innerHTML =
+    data.forecast.forecastday[1].day.mintemp_c + "°C";
+  document.querySelector(".tCond").innerHTML =
+    data.forecast.forecastday[1].day.condition.text;
 }
 
 function getAfterTomorrow(data) {
-    document.querySelector('.atIcon').src = data.forecast.forecastday[2].day.condition.icon;
-    document.querySelector('.atMaxTemp').innerHTML = data.forecast.forecastday[2].day.maxtemp_c+ "°C";
-    document.querySelector('.atMinTemp').innerHTML = data.forecast.forecastday[2].day.mintemp_c+ "°C";
-    document.querySelector('.atCond').innerHTML = data.forecast.forecastday[2].day.condition.text;
+  document.querySelector(".atIcon").src =
+    data.forecast.forecastday[2].day.condition.icon;
+  document.querySelector(".atMaxTemp").innerHTML =
+    data.forecast.forecastday[2].day.maxtemp_c + "°C";
+  document.querySelector(".atMinTemp").innerHTML =
+    data.forecast.forecastday[2].day.mintemp_c + "°C";
+  document.querySelector(".atCond").innerHTML =
+    data.forecast.forecastday[2].day.condition.text;
 }
 
-searchBar.addEventListener('keyup', function() {
-    var city = searchBar.value;
-    getData(city);
-})
+searchBar.addEventListener("keyup", function () {
+  var city = searchBar.value;
+  getData(city);
+});
 
-
-
-
-
-getData('cairo');
+getData("cairo");
